8871d0e8438fe954a76917f66ef71c982144d4f7
rkttu <rkttu@rkttu.com>
2014-05-28 00:57:19 +0900

    Added NUnit Test Case for demonstration Async Control Add/Remove added

---
+++ WindowsFormsAsyncExtension.Test/TestCase.cs
@@ -0,0 +1,137 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAsyncExtension.Test
+{
+    [TestFixture]
+    public sealed class TestCase
+    {
+        private Form testForm;
+        private Button testControl;
+        private Thread testThread;
+
+        [TestFixtureSetUp]
+        public void SetUp()
+        {
+            if (this.testForm != null)
+            {
+                try { this.testForm.Dispose(); }
+                catch { }
+
+                this.testForm = null;
+            }
+
+            this.testThread = new Thread(this.TestThread);
+            this.testThread.Start();
+        }
+
+        private void TestThread()
+        {
+            Application.OleRequired();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            this.testForm = new Form()
+            {
+                Text = DateTime.UtcNow.ToString()
+            };
+            this.testControl = new Button()
+            {
+                Text = "Click Here!"
+            };
+            Application.Run(this.testForm);
+        }
+
+        [TestFixtureTearDown]
+        public void TearDown()
+        {
+            if (this.testForm != null)
+            {
+                try { this.testForm.Dispose(); }
+                catch { }
+
+                this.testForm = null;
+            }
+
+            if (this.testThread != null)
+            {
+                try { this.testThread.Abort(); }
+                catch { }
+
+                this.testThread = null;
+            }
+        }
+
+        [Test]
+        public async Task ShowHideTest()
+        {
+            bool visibility;
+
+            await this.testForm.ShowAsync();
+            visibility = await this.testForm.GetVisibleAsync();
+            Assert.That(visibility, Is.True);
+
+            await this.testForm.SetTextAsync("Please wait a moment.");
+            Thread.Sleep(TimeSpan.FromSeconds(3d));
+
+            await this.testForm.HideAsync();
+            visibility = await this.testForm.GetVisibleAsync();
+            Assert.That(visibility, Is.False);
+        }
+
+        [Test]
+        public async Task GetSetTitleTest()
+        {
+            string temp;
+            bool visibility;
+
+            await this.testForm.ShowAsync();
+            visibility = await this.testForm.GetVisibleAsync();
+            Assert.That(visibility, Is.True);
+
+            await testForm.SetTextAsync("Three!");
+            temp = await testForm.GetTextAsync();
+            Assert.That(temp, Is.EqualTo("Three!"));
+            Thread.Sleep(TimeSpan.FromSeconds(1d));
+
+            await testForm.SetTextAsync("Two!");
+            temp = await testForm.GetTextAsync();
+            Assert.That(temp, Is.EqualTo("Two!"));
+            Thread.Sleep(TimeSpan.FromSeconds(1d));
+
+            await testForm.SetTextAsync("One!");
+            temp = await testForm.GetTextAsync();
+            Assert.That(temp, Is.EqualTo("One!"));
+            Thread.Sleep(TimeSpan.FromSeconds(1d));
+
+            await this.testForm.HideAsync();
+            visibility = await this.testForm.GetVisibleAsync();
+            Assert.That(visibility, Is.False);
+        }
+
+        [Test]
+        public async Task ControlAddRemoveTest()
+        {
+            bool visibility;
+
+            await this.testForm.ShowAsync();
+            visibility = await this.testForm.GetVisibleAsync();
+            Assert.That(visibility, Is.True);
+
+            Thread.Sleep(TimeSpan.FromSeconds(1d));
+            await this.testForm.AddControlAsync(this.testControl);
+            Thread.Sleep(TimeSpan.FromSeconds(1d));
+            await this.testForm.RemoveControlAsync(this.testControl);
+            Thread.Sleep(TimeSpan.FromSeconds(1d));
+
+            await this.testForm.HideAsync();
+            visibility = await this.testForm.GetVisibleAsync();
+            Assert.That(visibility, Is.False);
+        }
+    }
+}
---
+++ WindowsFormsAsyncExtension/WFAsyncExtensions_Controls.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Windows.Forms
+{
+    partial class WFAsyncExtensions
+    {
+        private static readonly Action<System.Windows.Forms.Control, System.Windows.Forms.Control> addControlInternal = (x, v) => x.Controls.Add(v);
+        public static void AddControl(this System.Windows.Forms.Control target, System.Windows.Forms.Control value)
+        {
+            if (target == null)
+                throw new ArgumentNullException("target");
+            if (target.InvokeRequired)
+                target.Invoke(addControlInternal, target, value);
+            else
+                addControlInternal.Invoke(target, value);
+        }
+        private static readonly Action<System.Windows.Forms.Control, System.Windows.Forms.Control> addControlCallback = AddControl;
+        public static IAsyncResult BeginAddControl(this System.Windows.Forms.Control target, System.Windows.Forms.Control value, AsyncCallback callback, object state)
+        {
+            return addControlCallback.BeginInvoke(target, value, callback, state);
+        }
+        public static void EndAddControl(this System.Windows.Forms.Control target, IAsyncResult result)
+        {
+            EndAddControl(result);
+        }
+        public static void EndAddControl(IAsyncResult result)
+        {
+            addControlCallback.EndInvoke(result);
+        }
+        public static Task AddControlAsync(this System.Windows.Forms.Control target, System.Windows.Forms.Control value)
+        {
+            return Task.Factory.FromAsync<System.Windows.Forms.Control, System.Windows.Forms.Control>(
+                BeginAddControl,
+                EndAddControl,
+                target, value, null);
+        }
+
+        private static readonly Action<System.Windows.Forms.Control, System.Windows.Forms.Control> removeControlInternal = (x, v) => x.Controls.Remove(v);
+        public static void RemoveControl(this System.Windows.Forms.Control target, System.Windows.Forms.Control value)
+        {
+            if (target == null)
+                throw new ArgumentNullException("target");
+            if (target.InvokeRequired)
+                target.Invoke(removeControlInternal, target, value);
+            else
+                removeControlInternal.Invoke(target, value);
+        }
+        private static readonly Action<System.Windows.Forms.Control, System.Windows.Forms.Control> removeControlCallback = RemoveControl;
+        public static IAsyncResult BeginRemoveControl(this System.Windows.Forms.Control target, System.Windows.Forms.Control value, AsyncCallback callback, object state)
+        {
+            return removeControlCallback.BeginInvoke(target, value, callback, state);
+        }
+        public static void EndRemoveControl(this System.Windows.Forms.Control target, IAsyncResult result)
+        {
+            EndRemoveControl(result);
+        }
+        public static void EndRemoveControl(IAsyncResult result)
+        {
+            removeControlCallback.EndInvoke(result);
+        }
+        public static Task RemoveControlAsync(this System.Windows.Forms.Control target, System.Windows.Forms.Control value)
+        {
+            return Task.Factory.FromAsync<System.Windows.Forms.Control, System.Windows.Forms.Control>(
+                BeginRemoveControl,
+                EndRemoveControl,
+                target, value, null);
+        }
+    }
+}
